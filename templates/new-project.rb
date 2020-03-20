81078765907de8d8c3cbec00b7e1d2d58c93b3a0
Ryan Strickler <ryan@littlekingsoftware.com>
2020-03-20 11:39:14 -0500

    Debug

--- templates/new-project.rb
+++ templates/new-project.rb
@@ -1,5 +1,7 @@
 app_name = ask("What is the name of this project?")
 
+puts "rails new #{app_name} -d postgresql --webpack=stimulus"
+
 rails_command "rails new #{app_name} -d postgresql --webpack=stimulus"
 rails_command "rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/ruby-version.rb"
 rails_command "rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/gemfile.rb"
