2cdae71f89d346e380a17211b6683464d6b5b203
Ryan Strickler <ryan@littlekingsoftware.com>
2020-03-30 17:51:35 -0500

    Try updating Gemfile before .ruby-version

--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -9,8 +9,8 @@ read app_name
 rails new $app_name -d postgresql --webpack=stimulus
 cd $app_name
 
-rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/ruby-version.rb
 rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/gemfile.rb
+rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/ruby-version.rb
 bundle
 
 git init
