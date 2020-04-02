8424b608ca83a2beba7e2c4101cbb3cbb65bce5e
Ryan Strickler <ryan@littlekingsoftware.com>
2020-04-02 11:11:12 -0500

    Fake change

--- templates/test_helper.rb
+++ templates/test_helper.rb
@@ -3,14 +3,6 @@ run 'rm test/test_helper.rb'
 file 'test/test_helper.rb', <<~CODE.strip_heredoc
   ENV['RAILS_ENV'] ||= 'test'
 
-  require 'simplecov'
-  SimpleCov.start 'rails' do
-    add_filter 'app/channels'
-    add_filter 'app/mailers'
-    add_filter 'app/jobs'
-    add_filter 'lib'
-  end
-
   require_relative '../config/environment'
   require 'rails/test_help'
   require 'webmock/minitest'
