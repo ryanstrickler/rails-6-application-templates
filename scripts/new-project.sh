3688604d4a3672a0b1aa1d7ee440d0feb139683c
Ryan Strickler <ryan@littlekingsoftware.com>
2020-04-02 11:45:32 -0500

    Build route/test for root URL

--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -11,6 +11,7 @@ cd $app_name
 
 rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/gemfile.rb
 rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/test_helper.rb
+rails app:template LOCATION=https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/templates/root.rb
 bundle
 
 rails db:create
---
+++ templates/root.rb
@@ -0,0 +1,30 @@
+run 'rm config/routes.rb'
+file 'config/routes', <<~CODE.strip_heredoc
+  Rails.application.routes.draw do
+    root to: 'root#index'
+  end
+CODE
+
+file 'app/controllers/root_controller.rb', <<~CODE.strip_heredoc
+  class RootController < ApplicationController
+    def index
+    end
+  end
+CODE
+
+file 'app/views/root/index.html.slim', <<~CODE.strip_heredoc
+  h1 Root
+CODE
+
+file 'test/controllers/root_controller_test.rb', <<~CODE.strip_heredoc
+  require 'test_helper'
+
+  class RootControllerTest < ActionDispatch::IntegrationTest
+    describe '#index' do
+      test 'page loads' do
+        get root_url
+        assert_response :ok
+      end
+    end
+  end
+CODE
