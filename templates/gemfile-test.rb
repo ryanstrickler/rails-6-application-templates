242b5256177b3415505429f81bc269b45c3ae473
Ryan Strickler <ryan@littlekingsoftware.com>
2020-04-15 11:42:12 -0500

    Fix method call

--- templates/gemfile-test.rb
+++ templates/gemfile-test.rb
@@ -3,4 +3,4 @@ def replace_file(filename:)
   file filename, open("https://raw.githubusercontent.com/ryanstrickler/rails-6-application-templates/master/files/#{filename}").read
 end
 
-replace_file('.ruby-version')
+replace_file(filename: '.ruby-version')
