155af8aeeb1386bec6d943e3dcf3772c373d2d9b
Ryan Strickler <ryan@littlekingsoftware.com>
2020-07-27 09:51:25 -0500

    Load or create device record on page load

--- files/app/controllers/application_controller.rb
+++ files/app/controllers/application_controller.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :check_device
+  before_action :load_or_create_device
 
   private
 
-  def check_device
-    return if cookies.encrypted[:device_id].present?
+  def load_or_create_device
+    load_device
+    return if @device.present?
+    create_device
+  end
 
-    device = Device.create!
-    cookies.encrypted[:device_id] = device.id
+  def load_device
+    return if cookies.encrypted[:device_id].blank?
+    @device = Device.find_by(id: cookies.encrypted[:device_id])
+  end
+
+  def create_device
+    @device = Device.create!
+    cookies.encrypted[:device_id] = @device.id
   end
 end
