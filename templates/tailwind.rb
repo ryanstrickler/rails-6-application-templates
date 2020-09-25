f6e02dc6650694b3b6ad1788b9e3f495bee92f46
Ryan Strickler <ryan@littlekingsoftware.com>
2020-09-25 15:13:11 -0500

    Add default application template

--- templates/tailwind.rb
+++ templates/tailwind.rb
@@ -18,3 +18,4 @@ end
 replace_file('postcss.config.js')
 add_file('app/javascript/stylesheets/application.scss')
 replace_file('app/javascript/packs/application.js')
+replace_file('app/views/layouts/application.html.erb')
