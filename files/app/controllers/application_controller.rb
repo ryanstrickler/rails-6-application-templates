00fb3ccd07cfcf5c0aecba80bef2446b991cde75
Ryan Strickler <ryan@littlekingsoftware.com>
2021-07-18 16:49:15 -0500

    Update device load

--- files/app/controllers/application_controller.rb
+++ files/app/controllers/application_controller.rb
@@ -5,23 +5,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_device
-    @current_device ||= load_or_create_device
-  end
-
   def load_or_create_device
-    load_device || create_device
-  end
-
-  def load_device
-    return if cookies.encrypted[:device_id].blank?
-
-    @current_device = Device.find_by(id: cookies.encrypted[:device_id])
-  end
-
-  def create_device
-    @current_device = Device.create!
+    @current_device = Device.find_or_create_by!(
+      id: cookies.encrypted[:device_id]
+    )
     cookies.encrypted[:device_id] = @current_device.id
-    @current_device
   end
 end
